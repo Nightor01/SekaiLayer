9bee62842d4a8af40b4fbe4c39956c96b3724dff
Lukáš Papoušek <papoulu@student.uzlabina.cz>
2025-10-11 15:09:07 +0200

    Added main project structure. Added a few icons. Added resource management. Added an AppTitle. Added minimization to systray.

---
+++ src/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace SekaiLayer;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    private readonly VaultManager _vaultManager;
+
+    public App()
+    {
+        InitializeComponent();
+        
+        _vaultManager = new ();
+    }
+    
+    private void App_Startup(object sender, StartupEventArgs e)
+    {
+        var startMinimized = e.Args.Contains("--minimized");
+
+        // TODO Make not appear upon start up
+        _vaultManager.Show();
+
+        if (startMinimized)
+        {
+            _vaultManager.Hide();
+        }
+    }
+}
+
---
+++ src/VaultManager.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace SekaiLayer;
+
+/// <summary>
+/// Interaction logic for VaultManager.xaml
+/// </summary>
+public partial class VaultManager : Window
+{
+    public VaultManager()
+    {
+        InitializeComponent();
+    }
+    
+    protected override void OnClosing(CancelEventArgs e)
+    {
+        Hide();
+        // We want to minimize to systray, not close the app
+        e.Cancel = true;
+    }
+
+    private void Open()
+    {
+        Show();
+        WindowState = WindowState.Normal;
+        Activate();
+    }
+
+    private void Exit()
+    {
+        TaskbarIcon.Dispose();
+        Application.Current.Shutdown();
+    }
+    
+    private void Open_OnClick(object sender, RoutedEventArgs e) => Open();
+    private void Exit_OnClick(object sender, RoutedEventArgs e) => Exit();
+    private void TaskbarIcon_OnTrayLeftMouseDoubleClick(object sender, RoutedEventArgs e) => Open();
+}
